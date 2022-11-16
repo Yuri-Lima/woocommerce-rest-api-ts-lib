a30608cf139cc5d54377eb30fa2a82efd9175470
Yuri-Lima <y.m.lima@hotmail.com>
2022-11-16 11:10:43 +0000

    Updated TypeScript lib

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,52 +1,34 @@
 import OAuth from "oauth-1.0a";
-import {
-  WooCommerceRestApiVersion,
-  WooCommerceRestApiEncoding,
-  WooCommerceRestApiMethod,
-  IWooCommerceRestApiOptions
-} from "./types";
+import { WooCommerceRestApiVersion, WooCommerceRestApiEncoding, WooCommerceRestApiMethod, IWooCommerceRestApiOptions } from "./types";
 export default class WooCommerceRestApi {
-  protected classVersion: string;
-  protected url: string;
-  protected consumerKey: string;
-  protected consumerSecret: string;
-  protected wpAPIPrefix: string;
-  protected version: WooCommerceRestApiVersion;
-  protected encoding: WooCommerceRestApiEncoding;
-  protected queryStringAuth: boolean;
-  protected port: number | string;
-  protected timeout: number;
-  protected axiosConfig: any;
-  protected isHttps: boolean;
-  constructor(opt: IWooCommerceRestApiOptions);
-  _setDefaultsOptions(opt: IWooCommerceRestApiOptions): void;
-  _parseParamsObject(params: any, query: any): any;
-  _normalizeQueryString(url: string, params?: any): string;
-  _getUrl(endpoint: string, params: Record<string, unknown>): string;
-  _getOAuth(): OAuth;
-  _request(
-    method: WooCommerceRestApiMethod,
-    endpoint: string,
-    data: Record<string, unknown>,
-    params?: Record<string, unknown>
-  ): Promise<any>;
-  get(endpoint: string, params?: Record<string, unknown>): Promise<any>;
-  post(
-    endpoint: string,
-    data: Record<string, unknown>,
-    params?: Record<string, unknown>
-  ): Promise<any>;
-  put(
-    endpoint: string,
-    data: Record<string, unknown>,
-    params?: Record<string, unknown>
-  ): Promise<any>;
-  delete(endpoint: string, params?: Record<string, unknown>): Promise<any>;
-  options(endpoint: string, params?: Record<string, unknown>): Promise<any>;
+    protected classVersion: string;
+    protected url: string;
+    protected consumerKey: string;
+    protected consumerSecret: string;
+    protected wpAPIPrefix: string;
+    protected version: WooCommerceRestApiVersion;
+    protected encoding: WooCommerceRestApiEncoding;
+    protected queryStringAuth: boolean;
+    protected port: number | string;
+    protected timeout: number;
+    protected axiosConfig: any;
+    protected isHttps: boolean;
+    constructor(opt: IWooCommerceRestApiOptions);
+    _setDefaultsOptions(opt: IWooCommerceRestApiOptions): void;
+    _parseParamsObject(params: any, query: any): any;
+    _normalizeQueryString(url: string, params?: any): string;
+    _getUrl(endpoint: string, params: Record<string, unknown>): string;
+    _getOAuth(): OAuth;
+    _request(method: WooCommerceRestApiMethod, endpoint: string, data: Record<string, unknown>, params?: Record<string, unknown>): Promise<any>;
+    get(endpoint: string, params?: Record<string, unknown>): Promise<any>;
+    post(endpoint: string, data: Record<string, unknown>, params?: Record<string, unknown>): Promise<any>;
+    put(endpoint: string, data: Record<string, unknown>, params?: Record<string, unknown>): Promise<any>;
+    delete(endpoint: string, params?: Record<string, unknown>): Promise<any>;
+    options(endpoint: string, params?: Record<string, unknown>): Promise<any>;
 }
 export declare class OptionsException {
-  name: "Options Error";
-  message: string;
-  constructor(message: string);
+    name: "Options Error";
+    message: string;
+    constructor(message: string);
 }
 //# sourceMappingURL=index.d.ts.map
--- dist/src/test.js
+++ dist/src/test.js
@@ -1,130 +1,126 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function(mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = __importDefault(require("./index"));
 const nock_1 = __importDefault(require("nock"));
 describe("#options", () => {
-  test("wpAPIPrefix should set WP REST API custom path", () => {
-    const api = new index_1.default({
-      url: "https://test.dev",
-      consumerKey: "ck_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-      consumerSecret: "cs_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-      wpAPIPrefix: "wp-rest",
-      version: "wc/v3"
+    test("wpAPIPrefix should set WP REST API custom path", () => {
+        const api = new index_1.default({
+            url: "https://test.dev",
+            consumerKey: "ck_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            consumerSecret: "cs_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            wpAPIPrefix: "wp-rest",
+            version: "wc/v3"
+        });
+        const endpoint = "products";
+        const expected = "https://test.dev/wp-rest/wc/v3/" + endpoint;
+        const url = api._getUrl(endpoint, {});
+        expect(url).toBe(expected);
     });
-    const endpoint = "products";
-    const expected = "https://test.dev/wp-rest/wc/v3/" + endpoint;
-    const url = api._getUrl(endpoint, {});
-    expect(url).toBe(expected);
-  });
 });
 describe("#methods", () => {
-  const api = new index_1.default({
-    url: "https://test.dev",
-    consumerKey: "ck_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    consumerSecret: "cs_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    version: "wc/v3"
-  });
-  test("_getUrl should return full endpoint URL", () => {
-    const endpoint = "products";
-    const expected = "https://test.dev/wp-json/wc/v3/" + endpoint;
-    const url = api._getUrl(endpoint, {});
-    expect(url).toBe(expected);
-  });
-  test("_normalizeQueryString should return query string sorted by name", () => {
-    const url =
-      "http://test.dev/wp-json/wc/v3/products?filter[q]=Woo+Album&fields=id&filter[limit]=1";
-    const expected =
-      "http://test.dev/wp-json/wc/v3/products?fields=id&filter[limit]=1&filter[q]=Woo%20Album";
-    const normalized = api._normalizeQueryString(url, {});
-    expect(normalized).toBe(expected);
-  });
+    const api = new index_1.default({
+        url: "https://test.dev",
+        consumerKey: "ck_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        consumerSecret: "cs_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        version: "wc/v3"
+    });
+    test("_getUrl should return full endpoint URL", () => {
+        const endpoint = "products";
+        const expected = "https://test.dev/wp-json/wc/v3/" + endpoint;
+        const url = api._getUrl(endpoint, {});
+        expect(url).toBe(expected);
+    });
+    test("_normalizeQueryString should return query string sorted by name", () => {
+        const url = "http://test.dev/wp-json/wc/v3/products?filter[q]=Woo+Album&fields=id&filter[limit]=1";
+        const expected = "http://test.dev/wp-json/wc/v3/products?fields=id&filter[limit]=1&filter[q]=Woo%20Album";
+        const normalized = api._normalizeQueryString(url, {});
+        expect(normalized).toBe(expected);
+    });
 });
 describe("#requests", () => {
-  beforeEach(() => {
-    nock_1.default.cleanAll();
-  });
-  const api = new index_1.default({
-    url: "https://test.dev",
-    consumerKey: "ck_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    consumerSecret: "cs_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    version: "wc/v3"
-  });
-  test("should return content for basic auth", () => {
-    expect.assertions(1);
-    (0, nock_1.default)(`https://test.dev/wp-json/wc/v3`)
-      .post("/orders", {})
-      .reply(201, {
-        ok: true
-      });
-    return api.post("orders", {}).then(response => {
-      expect(response.status).toBe(201);
+    beforeEach(() => {
+        nock_1.default.cleanAll();
     });
-  });
-  test("should return content for get requests", () => {
-    expect.assertions(1);
-    (0, nock_1.default)("https://test.dev/wp-json/wc/v3")
-      .get("/orders")
-      .reply(200, {
-        ok: true
-      });
-    return api.get("orders", {}).then(response => {
-      expect(response.status).toBe(200);
+    const api = new index_1.default({
+        url: "https://test.dev",
+        consumerKey: "ck_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        consumerSecret: "cs_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        version: "wc/v3"
     });
-  });
-  test("should return content for put requests", () => {
-    expect.assertions(1);
-    (0, nock_1.default)("https://test.dev/wp-json/wc/v3")
-      .put("/orders")
-      .reply(200, {
-        ok: true
-      });
-    return api.put("orders", {}).then(response => {
-      expect(response.status).toBe(200);
+    test("should return content for basic auth", () => {
+        expect.assertions(1);
+        (0, nock_1.default)(`https://test.dev/wp-json/wc/v3`)
+            .post("/orders", {})
+            .reply(201, {
+            ok: true
+        });
+        return api.post("orders", {}).then(response => {
+            expect(response.status).toBe(201);
+        });
     });
-  });
-  test("should return content for delete requests", () => {
-    expect.assertions(1);
-    (0, nock_1.default)("https://test.dev/wp-json/wc/v3")
-      .delete("/orders")
-      .reply(200, {
-        ok: true
-      });
-    return api.delete("orders", {}).then(response => {
-      expect(response.status).toBe(200);
+    test("should return content for get requests", () => {
+        expect.assertions(1);
+        (0, nock_1.default)("https://test.dev/wp-json/wc/v3")
+            .get("/orders")
+            .reply(200, {
+            ok: true
+        });
+        return api.get("orders", {}).then(response => {
+            expect(response.status).toBe(200);
+        });
     });
-  });
-  test("should return content for options requests", () => {
-    expect.assertions(1);
-    (0, nock_1.default)("https://test.dev/wp-json/wc/v3")
-      .intercept("/orders", "OPTIONS")
-      .reply(200, {
-        ok: true
-      });
-    return api.options("orders", {}).then(response => {
-      expect(response.status).toBe(200);
+    test("should return content for put requests", () => {
+        expect.assertions(1);
+        (0, nock_1.default)("https://test.dev/wp-json/wc/v3")
+            .put("/orders")
+            .reply(200, {
+            ok: true
+        });
+        return api.put("orders", {}).then(response => {
+            expect(response.status).toBe(200);
+        });
     });
-  });
-  test("should return content for OAuth", () => {
-    expect.assertions(1);
-    const oAuth = new index_1.default({
-      url: "http://test.dev",
-      consumerKey: "ck_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-      consumerSecret: "cs_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-      version: "wc/v3"
+    test("should return content for delete requests", () => {
+        expect.assertions(1);
+        (0, nock_1.default)("https://test.dev/wp-json/wc/v3")
+            .delete("/orders")
+            .reply(200, {
+            ok: true
+        });
+        return api.delete("orders", {}).then(response => {
+            expect(response.status).toBe(200);
+        });
     });
-    (0, nock_1.default)("http://test.dev/wp-json/wc/v3")
-      .filteringPath(/\?.*/, "?params")
-      .get("/orders?params")
-      .reply(200, {
-        ok: true
-      });
-    return oAuth.get("orders", {}).then(response => {
-      expect(response.status).toBe(200);
+    test("should return content for options requests", () => {
+        expect.assertions(1);
+        (0, nock_1.default)("https://test.dev/wp-json/wc/v3")
+            .intercept("/orders", "OPTIONS")
+            .reply(200, {
+            ok: true
+        });
+        return api.options("orders", {}).then(response => {
+            expect(response.status).toBe(200);
+        });
     });
-  });
+    test("should return content for OAuth", () => {
+        expect.assertions(1);
+        const oAuth = new index_1.default({
+            url: "http://test.dev",
+            consumerKey: "ck_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            consumerSecret: "cs_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            version: "wc/v3"
+        });
+        (0, nock_1.default)("http://test.dev/wp-json/wc/v3")
+            .filteringPath(/\?.*/, "?params")
+            .get("/orders?params")
+            .reply(200, {
+            ok: true
+        });
+        return oAuth.get("orders", {}).then(response => {
+            expect(response.status).toBe(200);
+        });
+    });
 });
