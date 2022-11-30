ba7fd46d56cfa71594d51ecf71771cb673e8bcfc
Yuri-Lima <y.m.lima@hotmail.com>
2022-11-30 18:52:02 +0000

    docs: general updates
    general Updates

--- src/test/test.ts
+++ src/test/test.ts
@@ -7,142 +7,142 @@ const secretKey = "ck_";
 const consumerKey = "cs_";
 
 describe("#options", () => {
-  test("wpAPIPrefix should set WP REST API custom path", () => {
-    const api = new WooCommerceRestApi({
-      url: "https://test.dev",
-      consumerKey: secretKey,
-      consumerSecret: consumerKey,
-      wpAPIPrefix: "wp-rest",
-      version: "wc/v3",
-      queryStringAuth: false, // Force Basic Authentication as query string true and using under HTTPS
+    test("wpAPIPrefix should set WP REST API custom path", () => {
+        const api = new WooCommerceRestApi({
+            url: "https://test.dev",
+            consumerKey: secretKey,
+            consumerSecret: consumerKey,
+            wpAPIPrefix: "wp-rest",
+            version: "wc/v3",
+            queryStringAuth: false, // Force Basic Authentication as query string true and using under HTTPS
+        });
+
+        const endpoint = "products";
+        const expected = "https://test.dev/wp-rest/wc/v3/" + endpoint;
+        const url = api._getUrl(endpoint, {});
+
+        expect(url).toBe(expected);
     });
-
-    const endpoint = "products";
-    const expected = "https://test.dev/wp-rest/wc/v3/" + endpoint;
-    const url = api._getUrl(endpoint, {});
-
-    expect(url).toBe(expected);
-  });
 });
 
 describe("#methods", () => {
-  const api = new WooCommerceRestApi({
-    url: "https://test.dev",
-    consumerKey: "secretKey",
-    consumerSecret: consumerKey,
-    version: "wc/v3",
-    queryStringAuth: false, // Force Basic Authentication as query string true and using under HTTPS
-  });
+    const api = new WooCommerceRestApi({
+        url: "https://test.dev",
+        consumerKey: "secretKey",
+        consumerSecret: consumerKey,
+        version: "wc/v3",
+        queryStringAuth: false, // Force Basic Authentication as query string true and using under HTTPS
+    });
 
-  test("_getUrl should return full endpoint URL", () => {
-    const endpoint = "products";
-    const expected = "https://test.dev/wp-json/wc/v3/" + endpoint;
-    const url = api._getUrl(endpoint, {});
+    test("_getUrl should return full endpoint URL", () => {
+        const endpoint = "products";
+        const expected = "https://test.dev/wp-json/wc/v3/" + endpoint;
+        const url = api._getUrl(endpoint, {});
 
-    expect(url).toBe(expected);
-  });
+        expect(url).toBe(expected);
+    });
 
-  test("_normalizeQueryString should return query string sorted by name", () => {
-    const url =
+    test("_normalizeQueryString should return query string sorted by name", () => {
+        const url =
       "http://test.dev/wp-json/wc/v3/products?filter[q]=Woo+Album&fields=id&filter[limit]=1";
-    const expected =
+        const expected =
       "http://test.dev/wp-json/wc/v3/products?fields=id&filter[limit]=1&filter[q]=Woo%20Album";
-    const normalized = api._normalizeQueryString(url, {});
+        const normalized = api._normalizeQueryString(url, {});
 
-    expect(normalized).toBe(expected);
-  });
+        expect(normalized).toBe(expected);
+    });
 });
 
 describe("#requests", () => {
-  beforeEach(() => {
-    nock.cleanAll(); // clean all nock mocks
-  });
-
-  const api = new WooCommerceRestApi({
-    url: "https://test.dev",
-    consumerKey: "secretKey",
-    consumerSecret: consumerKey,
-    version: "wc/v3",
-    queryStringAuth: false, // Force Basic Authentication as query string true and using under HTTPS
-  });
-
-  test("should return content for basic auth", () => {
-    expect.assertions(1);
-
-    nock(`https://test.dev/wp-json/wc/v3`).post("/orders", {}).reply(201, {
-      ok: true,
+    beforeEach(() => {
+        nock.cleanAll(); // clean all nock mocks
     });
 
-    return api.post("orders", {}).then((response: any) => {
-      expect(response.status).toBe(201);
-    });
-  });
-
-  test("should return content for get requests", () => {
-    expect.assertions(1);
-    nock("https://test.dev/wp-json/wc/v3").get("/orders").reply(200, {
-      ok: true,
+    const api = new WooCommerceRestApi({
+        url: "https://test.dev",
+        consumerKey: "secretKey",
+        consumerSecret: consumerKey,
+        version: "wc/v3",
+        queryStringAuth: false, // Force Basic Authentication as query string true and using under HTTPS
     });
 
-    return api.get("orders", {}).then((response: any) => {
-      expect(response.status).toBe(200);
-    });
-  });
+    test("should return content for basic auth", () => {
+        expect.assertions(1);
 
-  test("should return content for put requests", () => {
-    expect.assertions(1);
-    nock("https://test.dev/wp-json/wc/v3").put("/orders").reply(200, {
-      ok: true,
-    });
+        nock(`https://test.dev/wp-json/wc/v3`).post("/orders", {}).reply(201, {
+            ok: true,
+        });
 
-    return api.put("orders", {}).then((response: any) => {
-      expect(response.status).toBe(200);
-    });
-  });
-
-  test("should return content for delete requests", () => {
-    expect.assertions(1);
-    nock("https://test.dev/wp-json/wc/v3").delete("/orders").reply(200, {
-      ok: true,
+        return api.post("orders", {}).then((response: any) => {
+            expect(response.status).toBe(201);
+        });
     });
 
-    return api.delete("orders", {}).then((response: any) => {
-      expect(response.status).toBe(200);
-    });
-  });
+    test("should return content for get requests", () => {
+        expect.assertions(1);
+        nock("https://test.dev/wp-json/wc/v3").get("/orders").reply(200, {
+            ok: true,
+        });
 
-  test("should return content for options requests", () => {
-    expect.assertions(1);
-    nock("https://test.dev/wp-json/wc/v3")
-      .intercept("/orders", "OPTIONS")
-      .reply(200, {
-        ok: true,
-      });
-
-    return api.options("orders", {}).then((response: any) => {
-      expect(response.status).toBe(200);
-    });
-  });
-
-  test("should return content for OAuth", () => {
-    expect.assertions(1);
-    const oAuth = new WooCommerceRestApi({
-      url: "http://test.dev",
-      consumerKey: "secretKey",
-      consumerSecret: consumerKey,
-      version: "wc/v3",
-      queryStringAuth: false, // Force Basic Authentication as query string true and using under HTTPS
+        return api.get("orders", {}).then((response: any) => {
+            expect(response.status).toBe(200);
+        });
     });
 
-    nock("http://test.dev/wp-json/wc/v3")
-      .filteringPath(/\?.*/, "?params")
-      .get("/orders?params")
-      .reply(200, {
-        ok: true,
-      });
+    test("should return content for put requests", () => {
+        expect.assertions(1);
+        nock("https://test.dev/wp-json/wc/v3").put("/orders").reply(200, {
+            ok: true,
+        });
 
-    return oAuth.get("orders", {}).then((response: any) => {
-      expect(response.status).toBe(200);
+        return api.put("orders", {}).then((response: any) => {
+            expect(response.status).toBe(200);
+        });
     });
-  });
+
+    test("should return content for delete requests", () => {
+        expect.assertions(1);
+        nock("https://test.dev/wp-json/wc/v3").delete("/orders").reply(200, {
+            ok: true,
+        });
+
+        return api.delete("orders", {}).then((response: any) => {
+            expect(response.status).toBe(200);
+        });
+    });
+
+    test("should return content for options requests", () => {
+        expect.assertions(1);
+        nock("https://test.dev/wp-json/wc/v3")
+            .intercept("/orders", "OPTIONS")
+            .reply(200, {
+                ok: true,
+            });
+
+        return api.options("orders", {}).then((response: any) => {
+            expect(response.status).toBe(200);
+        });
+    });
+
+    test("should return content for OAuth", () => {
+        expect.assertions(1);
+        const oAuth = new WooCommerceRestApi({
+            url: "http://test.dev",
+            consumerKey: "secretKey",
+            consumerSecret: consumerKey,
+            version: "wc/v3",
+            queryStringAuth: false, // Force Basic Authentication as query string true and using under HTTPS
+        });
+
+        nock("http://test.dev/wp-json/wc/v3")
+            .filteringPath(/\?.*/, "?params")
+            .get("/orders?params")
+            .reply(200, {
+                ok: true,
+            });
+
+        return oAuth.get("orders", {}).then((response: any) => {
+            expect(response.status).toBe(200);
+        });
+    });
 });
